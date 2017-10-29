34ac8c1b45d134b16a4438bc403e7cb62c9b7352
Shota Miyazaki <jmsf0203@gmail.com>
2017-10-29 10:31:19 +0900

    Add siebar ほか

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show] do
   end
 
+  resources :recipes, only: [:show] do
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
